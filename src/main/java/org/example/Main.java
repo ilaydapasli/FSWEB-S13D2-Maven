f7da1f517fb725b3f0c98888f5ce357604569ffa
ilaydapasli <ilayda.pasli2018@stu.pirireis.edu.tr>
2025-01-07 14:40:14 +0300

    İLK SORU

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,28 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(isPalindrome(4554));
     }
+
+    public static boolean isPalindrome(int number) {
+
+
+        int originalNumber = number;
+        int reversedNumber = 0;
+
+        // Sayıyı ters çevir
+        while (number > 0) {
+            int digit = number % 10; // Son basamağı al
+            reversedNumber = reversedNumber * 10 + digit; // Ters sayıyı oluştur
+            number /= 10; // Sayının son basamağını sil
+        }
+
+        if (originalNumber == reversedNumber){
+            return true;
+        }
+        return false;
+    }
+
+
 }
+
